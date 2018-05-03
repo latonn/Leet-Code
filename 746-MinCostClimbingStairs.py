027a3b431668a5716adde8ab4da96d0813ef1538
latonn <wannafly0110@gmail.com>
2018-05-03 19:24:31 +0800

    update self solution

---
+++ 746-MinCostClimbingStairs.py
@@ -0,0 +1,50 @@
+#
+
+# On a staircase, the i-th step has some non-negative cost cost[i] assigned (0 indexed).
+#
+# Once you pay the cost, you can either climb one or two steps. You need to find minimum cost to
+# reach the top of the floor, and you can either start from the step with index 0, or the step with index 1.
+#
+# Example 1:
+# Input: cost = [10, 15, 20]
+# Output: 15
+# Explanation: Cheapest is start on cost[1], pay that cost and go to the top.
+#
+# Example 2:
+# Input: cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+# Output: 6
+# Explanation: Cheapest is start on cost[0], and only step on 1s, skipping cost[3].
+#
+# Note:
+# cost will have a length in the range [2, 1000].
+# Every cost[i] will be an integer in the range [0, 999].
+
+
+class Solution(object):
+    def minCostClimbingStairs(self, cost):
+        """
+        :type cost: List[int]
+        :rtype: int
+        """
+        # latonn's
+        if len(cost) == 1:
+            return 0
+
+        dp = [0 for _ in range(len(cost))]
+        dp[0], dp[1] = cost[0], cost[1]
+
+        for i in range(2, len(cost)):
+            dp[i] = min(dp[i-1], dp[i-2]) + cost[i]
+
+        return min(dp[-1], dp[-2])
+
+        # kamyu's
+        # dp = [0] * 3
+        # for i in reversed(xrange(len(cost))):
+        #     dp[i%3] = cost[i] + min(dp[(i+1)%3], dp[(i+2)%3])
+        # return min(dp[0], dp[1])
+
+
+if __name__ == '__main__':
+    cost = [1, 100, 1, 1, 1, 100, 1, 1, 100, 1]
+    print(Solution().minCostClimbingStairs(cost))
