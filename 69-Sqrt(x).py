1f0d0977f4103ea557270209c8b4b7e573c91875
latonn <wannafly0110@gmail.com>
2018-06-12 22:29:09 +0800

    update solution

---
+++ 69-Sqrt(x).py
@@ -0,0 +1,47 @@
+# Time:  O(logn)
+# Space: O(1)
+
+# Implement int sqrt(int x).
+#
+# Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+#
+# Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is
+# returned.
+#
+# Example 1:
+#
+# Input: 4
+# Output: 2
+# Example 2:
+#
+# Input: 8
+# Output: 2
+# Explanation: The square root of 8 is 2.82842..., and since
+#              the decimal part is truncated, 2 is returned.
+
+
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        # latonn's
+        # binary search
+        if x < 2:
+            return x
+
+        left, right = 1, x // 2
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid ** 2 > x:
+                right = mid - 1
+            else:
+                left = mid + 1
+
+        return left - 1
+
+
+if __name__ == '__main__':
+    x = 8
+    print(Solution().mySqrt(x))
