09f0159d9e498559f1bc9ef267e05681ab439f18
latonn <wannafly0110@gmail.com>
2018-07-07 23:38:58 +0800

    update solution

---
+++ BestTimetoBuyandSellStockII.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    # @param prices, a list of integer
+    # @return an integer
+    def maxProfit(self, prices):
+        maxprofit = 0
+        for i in range(1, len(prices)):
+            if prices[i] >= prices[i-1]:
+                maxprofit += prices[i] - prices[i-1]
+        return maxprofit
+
+
+
+
