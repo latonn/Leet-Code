25752c80df2a16227bc1e4b582ae35e03fc2d8ea
latonn <wannafly0110@gmail.com>
2018-07-08 19:30:28 +0800

    update solution

---
+++ SearchInsertPosition.py
@@ -0,0 +1,19 @@
+class Solution(object):
+        # @param A, a list of integers
+        # @param target, an integer to be inserted
+        # @return integer
+        def searchInsert(self, A, target):
+            left = 0; right = len(A) - 1
+            while left <= right:
+                mid = ( left + right ) / 2
+                if A[mid] < target:
+                    left = mid + 1
+                elif A[mid] > target:
+                    right = mid - 1
+                else:
+                    return mid
+            return left
+
+
+
+
