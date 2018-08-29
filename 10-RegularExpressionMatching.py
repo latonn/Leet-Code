c03503e22fad51edf7bf94c7c250debf0800d3d1
latonn <wannafly0110@gmail.com>
2018-08-28 21:56:03 -0700

    update solution

---
+++ 10-RegularExpressionMatching.py
@@ -0,0 +1,80 @@
+#
+
+# Given an input string (s) and a pattern (p), implement regular expression matching with support for '.' and '*'.
+#
+# '.' Matches any single character.
+# '*' Matches zero or more of the preceding element.
+# The matching should cover the entire input string (not partial).
+#
+# Note:
+#
+# s could be empty and contains only lowercase letters a-z.
+# p could be empty and contains only lowercase letters a-z, and characters like . or *.
+#
+# Example 1:
+# Input:
+# s = "aa"
+# p = "a"
+# Output: false
+# Explanation: "a" does not match the entire string "aa".
+#
+# Example 2:
+# Input:
+# s = "aa"
+# p = "a*"
+# Output: true
+# Explanation: '*' means zero or more of the precedeng element, 'a'. Therefore, by repeating 'a' once, it becomes "aa".
+#
+# Example 3:
+# Input:
+# s = "ab"
+# p = ".*"
+# Output: true
+# Explanation: ".*" means "zero or more (*) of any character (.)".
+#
+# Example 4:
+# Input:
+# s = "aab"
+# p = "c*a*b"
+# Output: true
+# Explanation: c can be repeated 0 times, a can be repeated 1 time. Therefore it matches "aab".
+#
+# Example 5:
+# Input:
+# s = "mississippi"
+# p = "mis*is*p*."
+# Output: false
+
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        dp = [[False for _ in range(len(p)+1)] for _ in range(len(s)+1)]
+        dp[0][0] = True
+        for i in range(1, len(p)+1):
+            if p[i-1] == "*" and i >= 2:
+                dp[0][i] = dp[0][i-2]
+        # print("Initial: ", dp)
+
+        for i in range(1, len(s)+1):
+            # print("i = ", i, " ", dp[i])
+            for j in range(1, len(p)+1):
+                if p[j-1] == '.':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i][j-1] or dp[i][j-2] or (dp[i-1][j] and (s[i-1] == p[j-2] or p[j-2] == "."))
+                else:
+                    dp[i][j] = dp[i-1][j-1] and s[i-1] == p[j-1]
+                # print("j = ", j, " ", dp[i])
+        return dp[len(s)][len(p)]
+
+
+if __name__ == "__main__":
+    s = "aab"
+    p = "c*a*b"
+    print(Solution().isMatch(s, p))
+
